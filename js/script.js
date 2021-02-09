d59ddaa7e9175cae31ab138b1e5b5296571fbe3f
Chris Clements <chrisclementss@gmail.com>
2021-02-09 13:39:44 -0500

    latest commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,55 @@
 let keyboard = document.querySelector('.piano__keyboard');
+let controls = document.querySelectorAll('.piano__control_option');
 let pianoNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+let keyboardMap = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+];
+let keys = [];
 
 let init = () => {
-    for(let i = 1; i <= 5; i++) {
-        for(let j = 0; j < 7; j++) {
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 0; j < 7; j++) {
             let key = createKey('white', pianoNotes[j], i);
+            key.dataset.keyboard = keyboardMap[j + (i - 1) * 7];
             keyboard.appendChild(key);
 
-            if(j != 2 && j != 6) {
+            if (j != 2 && j != 6) {
                 key = createKey('black', pianoNotes[j], i);
+                key.dataset.keyboard = '⇧+' + keyboardMap[j + (i - 1) * 7];
                 let emptySpace = document.createElement('div');
                 emptySpace.className = 'empty-space';
                 emptySpace.appendChild(key);
@@ -16,33 +57,56 @@ let init = () => {
             }
         }
     }
-}
+};
 
 let createKey = (type, note, octave) => {
     let key = document.createElement('button');
     key.className = `piano__key piano__key--${type}`;
-    key.dataset.letterNote = type == 'white' ? note + octave : note + '#' + octave;
-    key.dataset.letterNoteFileName = type == 'white' ? note + octave : note + 's' + octave;
+    key.dataset.letterNote =
+        type == 'white' ? note + octave : note + '#' + octave;
+    key.dataset.letterNoteFileName =
+        type == 'white' ? note + octave : note + 's' + octave;
     key.textContent = key.dataset.letterNote;
+    keys.push(key);
 
     key.addEventListener('mousedown', () => {
         playSound(key);
         key.classList.add('piano__key--playing');
-    })
+    });
     key.addEventListener('mouseup', () => {
         key.classList.remove('piano__key--playing');
-    })
+    });
     key.addEventListener('mouseleave', () => {
         key.classList.remove('piano__key--playing');
-    })
+    });
 
     return key;
-}
+};
 
 let playSound = (key) => {
     let audio = document.createElement('audio');
     audio.src = 'sounds/' + key.dataset.letterNoteFileName + '.mp3';
     audio.play().then(() => audio.remove());
-}
+};
+
+controls.forEach((input) => {
+    input.addEventListener('input', () => {
+        let value = input.value;
+        let type;
+        switch(value) {
+            case 'letterNotes':
+                type = 'letterNote';
+                break;
+            case 'keyboard':
+                type = 'keyboard';
+                break;
+            case 'none':
+                type = '';
+        }
+        keys.forEach((key) => {
+            key.textContent = key.dataset[type];
+        });
+    });
+});
 
 init();
